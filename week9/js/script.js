b3dd449a550023b3dc66d2aa6114d543948bb841
OlgaV1982 <98696812+OlgaV1982@users.noreply.github.com>
2023-05-08 17:55:11 +0800

    Add files via upload

---
+++ week9/js/script.js
@@ -0,0 +1,21 @@
+console.log('Hello world!')
+/*let n = (40000+22000);*/
+console.log(40000+22000);
+console.log('Владивосток'+' - 2000');
+console.log((128+254+137+201)/60);
+console.log(2**5);
+console.log('Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!');
+const greetings = "Привет, ";
+const name = "Кот";
+console.log(greetings+name);
+let time = 34;
+console.log('Старт поездки. Осталось минут: '+ time);
+time = 34-15;
+console.log('Немного сториз в соцсетях. Осталось минут: '+ time);
+time = 19-10;
+console.log('Немного не новостей, но событий. Осталось минут: '+ time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву.');
+let fahrenheit = 541;
+let celsius  = (fahrenheit - 32) / 1.8;
+console.log(`541 градус по Фаренгейту - это ${(fahrenheit - 32) / 1.8} градусов по Цельсию`);
